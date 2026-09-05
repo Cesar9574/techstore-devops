4b8df705da662f522f73348a4499a22c3801435f
Cesar9574 <cgonzalezg30@miumg.edu.gt>
2026-09-05 08:15:40 -0600

    Migrar credenciales a variables de entorno y limpiar TODOs con referencia a Issues

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 function calcularTotal(precio, cantidad) {
-    // TODO: agregar validación de precios negativos
+    // TODO: agregar validacion de precios negativos (ver Issue #1)
     return precio * cantidad;
 }
 
 function aplicarDescuento(total, porcentaje) {
-    // TODO: validar que porcentaje esté entre 0 y 100
+    // TODO: validar que porcentaje este entre 0 y 100 (ver Issue #2)
     return total - (total * porcentaje / 100);
 }
 
